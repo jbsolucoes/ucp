498f4f73e3d51f763fe0449f85a1a72b7b573a3b
jbsolucoes <josebenedito@gmail.com>
2015-11-05 01:30:59 -0200

    fix generate resourses components icon

--- package/images/make.bat
+++ package/images/make.bat
@@ -1,1 +1,3 @@
 c:\lazarus\tools\lazres ..\lazarus\usercontrol.lrs TUserControl.bmp TUCControls.bmp TUCIdle.bmp TMailUserControl.bmp TUCSettings.bmp TUCApplicationMessage.bmp
+c:\lazarus\tools\lazres ..\connectors\ucsqldbconn\ucsqldbconn.lrs TUCSQLdbConn.bmp
+c:\lazarus\tools\lazres ..\connectors\uczeosconn\uczeosconn.lrs TUCZEOSConn.bmp
